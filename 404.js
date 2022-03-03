2181ea60be8eb2a16264cdacb45891947831ebec
aandroxus1 <97003411+aandroxus1@users.noreply.github.com>
2022-03-03 21:54:30 +0000

    Idk

--- 404.js
+++
@@ -1,5 +0,0 @@
-function anim_expandOptions(){
-    elem=document.getElementById("option1")
-    elem.style.animation = ("0.7s expandOption")
-    setTimeout(elem.style.left = "10%",700);
-}
--- app.js
+++ app.js
@@ -1,21 +1,3 @@
 function defTest(){
     alert("button1");
 }
-function anim_sidebarOut(){
-    elem=document.getElementById("sidebar")
-    elem.style.animation = ("0.7s sidebarOut");
-    elem.style.left = "-17%";
-}
-function anim_sidebarIn(){
-    elem=document.getElementById("sidebar")
-    elem.style.animation = ("0.7s sidebarIn");
-    elem.style.left = "0%";
-}
-function anim_ico_onHover(){
-    elem=document.getElementById("sidebarIco")
-    elem.style.animation=("0.7s rotateHover")
-}
-function anim_ico_onHoverRelease(){
-    elem=document.getElementById("sidebarIco")
-    elem.style.animation=("0.7s rotateRelease")
-}
