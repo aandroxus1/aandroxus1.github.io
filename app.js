3f76466ca08d176fe782abbce34541962a06f98d
aandroxus1 <97003411+aandroxus1@users.noreply.github.com>
2022-03-03 10:03:26 +0000

    Img Upload

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function defTest(){
     alert("button1");
 }
+function anim_sidebarOut(){
+    elem=document.getElementById("sidebar")
+    elem.style.animation = ("0.7s sidebarOut");
+    elem.style.left = "-17%";
+}
+function anim_sidebarIn(){
+    elem=document.getElementById("sidebar")
+    elem.style.animation = ("0.7s sidebarIn");
+    elem.style.left = "0%";
+}
+console.log(document.getElementById("sidebar").attributes);
