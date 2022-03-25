c55951ae4c6f6dea978dcddd89f664413335043b
aandroxus1 <97003411+aandroxus1@users.noreply.github.com>
2022-03-25 10:08:39 +0000

    Monopoly Update

---
+++ chess/app.js
@@ -0,0 +1,33 @@
+class Player{
+    constructor(location, money, properties){
+        self.location = location;
+        self.money = money;
+        self.properties = properties;
+    }
+}
+
+//Initialises all players, which contains location, properties held and money in player's balance
+
+function playerGenerate(){
+    let playerList = [];
+    /*
+    let playerCount = document.getElementById("playerCount").innerHTML;
+    console.log(playerCount)
+    console.log(document.getElementById("playerCount").innerHTML)
+    for (let i of (playerCount)){
+        playerList[i] = new Player;
+    }
+    console.log(playerList)
+    *///This code is not working, but I want to move on with actually building the game. Temporary hard player lock will be at 2.
+    for (let i of [1,2,3,4]){
+        playerList[i] = new Player;
+        console.log(playerList)
+    }
+}
+
+function rollDice(){
+    let dice=Math.random()
+    dice = dice * 6
+    dice = Math.ceil(dice)
+    document.getElementById("testlabel").innerHTML=dice
+}
